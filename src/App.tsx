0241227b069f07a000c705920d73ebe425be09ef
Blinky <matthewmakary@trentu.ca>
2024-04-04 20:38:05 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import glow from "./assets/glow.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-white underline">Hello world!</h1>
+    </>
+  );
+}reesr
+
+export default App;
